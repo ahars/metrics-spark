460feb25d6fedda896f3a41771b97dec114ce9d5
ahars <ahars@ippon.fr>
2014-12-09 15:14:05 +0100

    init test metricsreceiver

---
+++ src/main/java/fr/ippontech/metrics/spark/streaming/MetricsToConsole.java
@@ -0,0 +1,26 @@
+package fr.ippontech.metrics.spark.streaming;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+
+public class MetricsToConsole {
+
+    public static void main(String[] args) {
+
+        SparkConf conf = new SparkConf()
+            .setAppName("MetricsToConsole")
+            .setMaster("local[2]");
+
+        JavaStreamingContext ssc = new JavaStreamingContext(conf, new Duration(5000));
+
+        ssc.receiverStream(new MetricsReceiver("localhost", 9999))
+            .foreachRDD(rdd -> {
+                rdd.foreach(measure -> System.out.println(measure.toString()));
+                return null;
+            });
+
+        ssc.start();
+        ssc.awaitTermination();
+    }
+}
